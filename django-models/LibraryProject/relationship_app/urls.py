f495fc0d592727523cee7900082c2968fe1a1351
Wepngong Shalom <wepngongshalom23@gmail.com>
2025-02-23 22:19:44 +0100

    correction

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -15,5 +15,7 @@ urlpatterns = [
     path('logout/', 
          LogoutView.as_view(template_name='relationship_app/logout.html'), 
          name='logout'),
-    
+    path('books/add/', add_book, name='add_book'),
+    path('books/edit/<int:book_id>/', edit_book, name='edit_book'),
+    path('books/delete/<int:book_id>/', delete_book, name='delete_book'),
 ]
