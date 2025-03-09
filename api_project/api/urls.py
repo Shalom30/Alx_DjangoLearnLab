8e9b0039b89a83fdb1784125ac5326f1e7f6a051
Wepngong Shalom <wepngongshalom23@gmail.com>
2025-03-09 17:40:57 +0100

    correction 5

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import BookViewSet  # Remove BookList import
+from .views import BookList
 
 router = DefaultRouter()
 router.register(r'books', BookViewSet, basename='book')
 
 urlpatterns = [
     path('', include(router.urls)),  # Only include the router-generated URLs
+    path('books/', BookList.as_view(), name='book-list')
 ]
 
