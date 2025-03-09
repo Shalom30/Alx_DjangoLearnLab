0dc2eaa40c62a7f303a12c1dfd959d1d076ca688
Wepngong Shalom <wepngongshalom23@gmail.com>
2025-03-09 17:26:47 +0100

    work 2

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .serializers import BookSerializers
+from .models import Book
+from rest_framework import viewsets
+from .permissions import IsAdminOrReadOnly
+class BookViewSet(viewsets.ModelViewSet):
+  queryset = Book.objects.all()
+  serializer_class = BookSerializers
+  permission_classes = [IsAdminOrReadOnly]
