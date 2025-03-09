a45b05eb69d08ef67300d4cd39741cafed5ee7fd
Wepngong Shalom <wepngongshalom23@gmail.com>
2025-03-09 17:59:31 +0100

    task 3.2

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,11 +1,15 @@
 from rest_framework import generics, viewsets
 from .models import Book
 from .serializers import BookSerializers
+from .permissions import IsAdminOrReadOnly
+from rest_framework.permissions import IsAuthenticated
 
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializers
+    permission_classes = [IsAdminOrReadOnly]
 
 class BookViewSet(viewsets.ModelViewSet):
   queryset = Book.objects.all()
   serializer_class = BookSerializers
+  permission_classes = [IsAuthenticated]
